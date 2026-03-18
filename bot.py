a8031bfe667ac2d80093ba139e63ec2bdbced336
yarmukhamedovsergey-creator <yarmukhamedovsergey@gmail.com>
2026-03-18 22:13:45 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,6 @@ ACCOUNTS = [
     {"api_id": 36360664, "api_hash": "facb9902e2eafe009a2fb43c901c2328", "phone": "+959694410210"},
     {"api_id": 38047070, "api_hash": "8132d885c41d0db88c345a868de305e5", "phone": "+15029264416"},
     {"api_id": 37487174, "api_hash": "65168477b7764d3163d8a3b2bc3e9006", "phone": "+18633585097"},
-    {"api_id": 36689382, "api_hash": "68e81cc3a654222750230c8bafff6c2c", "phone": "+18598807376"},
 ]
 
 FREE_SEARCHES = 3
