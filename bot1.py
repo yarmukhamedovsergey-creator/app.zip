d003ea40b0c9585d81909e81e8b9a4f3721fba78
yarmukhamedovsergey-creator <yarmukhamedovsergey@gmail.com>
2026-05-22 23:01:45 +0500

    Update and rename fix_db.py to bot1.py

---
+++ bot1.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/env python3
+import subprocess, sys, os
+
+BOT_PATH='/root/bot.py'
+VENV_PATH='/root/venv/bin/activate'
+
+# Функция проверяет запущен ли бот
+def is_bot_running():
+    result = subprocess.run(['pgrep', '-f', BOT_PATH], stdout=subprocess.PIPE)
+    return bool(result.stdout.strip())
+
+# Если запущен, ничего не делаем
+if is_bot_running():
+    print('Бот уже запущен, пропуск.')
+    sys.exit(0)
+
+# Активируем виртуальное окружение
+activate_cmd = f'source {VENV_PATH}'
+subprocess.call(activate_cmd, shell=True, executable='/bin/bash')
+
+# Запускаем бот в фоне с логированием
+log_file='/root/bot.log'
+subprocess.Popen([sys.executable, BOT_PATH], stdout=open(log_file,'a'), stderr=subprocess.STDOUT)
+print('Бот запущен.')
